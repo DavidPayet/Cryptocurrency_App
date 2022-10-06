17d68865532e6988d62b2d16721224c6c8d969b5
David Payet <davidpayet570@gmail.com>
2022-10-06 17:27:21 +0200

    routes, links ans all necessary components initialization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+import './style/App.css'
+import { Navbar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Cryptocurrency App</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/exchanges' element={<Exchanges />} />
+              <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+              <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+              <Route path='/news' element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptocurrency App
+            <br />
+            All rights reserverd
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
